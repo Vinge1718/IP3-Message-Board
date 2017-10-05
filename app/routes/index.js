5bd6472900f414972690ecb62ae315b40dc2d372
Victor King'e <vinge1718@gmail.com>
2017-10-05 16:18:28 +0300

    added the corresponding files to the update component template and component js file

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,16 @@ export default Ember.Route.extend({
   },
 
   actions:{
+    update(message, params){
+      Object.keys(params).forEach(function(key){
+        if(params[key]!==undefined){
+          message.set(key,params[key]);
+        }
+      });
+      message.save();
+      this.transitionTo("index");
+    },
+
     destroyMessage(message){
       message.destroyRecord();
       this.transitionTo("index");
